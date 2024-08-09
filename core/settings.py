1523ddd22cff2546aafca2db83606effeef67ed2
Samantaks <saksamantha@gmail.com>
2024-08-09 12:13:25 -0300

    Começo da página de cadastro

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from django.contrib.messages import constants
 from pathlib import Path
 import os
 
@@ -128,3 +128,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Mensagens de Alerta
+
+MESSAGE_TAGS = {
+ constants.DEBUG: 'alert-primary',
+ constants.ERROR: 'alert-danger',
+ constants.SUCCESS: 'alert-success',
+ constants.INFO: 'alert-info',
+ constants.WARNING: 'alert-warning',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if senha!= confirmar_senha:
+            return HttpResponse("Senhas não conferem!")
+
+        if len(senha)<6:
+            return HttpResponse("Senha muito curta!")
+        
+        if User.objects.filter(username=username).exists():
+            return HttpResponse("Usuário já existe!")
+        
+        user = User.objects.create_user(username=username, password=senha)
+        user.save()
+        return redirect('usuarios/logar')
