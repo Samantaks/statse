2d0ae5a6676324088ce979c42243b5bcd74940a1
Samantaks <saksamantha@gmail.com>
2024-08-13 12:24:39 -0300

    app empresarios iniciado

--- empresarios/views.py
+++ empresarios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def cadastrar_empresa(request):
+    if request.method == "GET":
+        return render(request, 'cadastrar_empresa.html')
